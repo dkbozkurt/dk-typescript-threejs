495f22b92f9bf5f1c0397e458074aa4b6e77d7b0
Dogukan Bozkurt <dkaanbozkurt@gmail.com>
2024-02-15 17:06:06 +0100

    Scripts back to original

--- Three.js-TypeScript-Tutorial/src/client/client.ts
+++ Three.js-TypeScript-Tutorial/src/client/client.ts
@@ -2,87 +2,37 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene()
-const scene2 = new THREE.Scene()
 
-scene.background = new THREE.Color(0x000000);
-scene.background = new THREE.Color(0x000000);
-
-// https://sbcode.net/threejs/scene-camera-renderer/
-const camera1 = new THREE.PerspectiveCamera(
-    75, // FOV
-    1, // Change here for a specific width-height values exp:200/200 : Aspect Ratio
-    0.1, // Near plane clip
-    1000 // Far plane clip
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
 )
+camera.position.z = 2
 
-const camera2 = new THREE.OrthographicCamera(
-    -2,2,2,-2 , // Camera size in Unity
-    .1, // Near plane clip
-    10, // Far plane clip
-)
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 
-const camera3 = new THREE.OrthographicCamera(
-    -2,2,2,-2 , // Camera size in Unity
-    .1, // Near plane clip
-    10, // Far plane clip
-)
+new OrbitControls(camera, renderer.domElement)
 
-const camera4 = new THREE.OrthographicCamera(
-    -2,2,2,-2 , // Camera size in Unity
-    .1, // Near plane clip
-    10, // Far plane clip
-)
-camera1.position.z = 2
-camera2.position.y = 2
-camera2.lookAt(new THREE.Vector3())
-camera3.position.x = -2
-camera3.lookAt(new THREE.Vector3())
-camera4.position.z = 2
-
-const canvas1 = document.getElementById("c1") as HTMLCanvasElement;
-const canvas2 = document.getElementById("c2") as HTMLCanvasElement;
-const canvas3 = document.getElementById("c3") as HTMLCanvasElement;
-const canvas4 = document.getElementById("c4") as HTMLCanvasElement;
-
-const renderer1 = new THREE.WebGLRenderer({canvas: canvas1});
-renderer1.setSize(200,200) // Change here for a specific width-height values exp:renderer.setSize(200,200)
-// document.body.appendChild(renderer.domElement)
-const renderer2 = new THREE.WebGLRenderer({canvas: canvas2});
-renderer2.setSize(200,200);
-const renderer3 = new THREE.WebGLRenderer({canvas: canvas3});
-renderer2.setSize(200,200);
-const renderer4 = new THREE.WebGLRenderer({canvas: canvas4});
-renderer2.setSize(200,200);
-
-new OrbitControls(camera1, renderer1.domElement)
-
-const geometry = new THREE.TorusKnotGeometry()
+const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
     color: 0x00ff00,
     wireframe: true,
 })
 
 const cube = new THREE.Mesh(geometry, material)
-cube.scale.x = .5
-cube.scale.y = .5
-cube.scale.z = .5
 scene.add(cube)
-const cube2 = new THREE.Mesh(geometry, material)
-scene2.add(cube2)
 
-console.dir(scene);
-console.log(scene);
-
-
-// This part makes the screen responsive!!!
-// Disable for static screen sizes
-// window.addEventListener('resize', onWindowResize, false)
-// function onWindowResize() {
-//     camera.aspect = window.innerWidth / window.innerHeight
-//     camera.updateProjectionMatrix()
-//     renderer.setSize(window.innerWidth, window.innerHeight)
-//     render()
-// }
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    render()
+}
 
 function animate() {
     requestAnimationFrame(animate)
@@ -90,16 +40,11 @@ function animate() {
     cube.rotation.x += 0.01
     cube.rotation.y += 0.01
 
-    cube2.rotation.y += 0.01
-
     render()
 }
 
 function render() {
-    renderer1.render(scene, camera1)
-    renderer2.render(scene, camera2)
-    renderer3.render(scene2, camera3)
-    renderer4.render(scene, camera4)
+    renderer.render(scene, camera)
 }
 
 animate()
