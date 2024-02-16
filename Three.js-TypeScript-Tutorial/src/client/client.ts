546d6bb9d35019ff871bf07b1ecbd55a00952c4d
Dogukan Bozkurt <dkaanbozkurt@gmail.com>
2024-02-16 16:03:06 +0100

    Material - start scripts

--- Three.js-TypeScript-Tutorial/src/client/client.ts
+++ Three.js-TypeScript-Tutorial/src/client/client.ts
@@ -12,9 +12,7 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
-camera.position.x = -2
-camera.position.y = 4
-camera.position.z = 5
+camera.position.z = 3
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -24,24 +22,35 @@ new OrbitControls(camera, renderer.domElement)
 
 const boxGeometry = new THREE.BoxGeometry()
 const sphereGeometry = new THREE.SphereGeometry()
-const icosahedronGeometry = new THREE.IcosahedronGeometry()
+const icosahedronGeometry = new THREE.IcosahedronGeometry(1, 0)
+const planeGeometry = new THREE.PlaneGeometry()
+const torusKnotGeometry = new THREE.TorusKnotGeometry()
 
-//console.dir(boxGeometry)
 const material = new THREE.MeshBasicMaterial({
     color: 0x00ff00,
     wireframe: true,
 })
+//const material= new THREE.MeshNormalMaterial()
 
 const cube = new THREE.Mesh(boxGeometry, material)
 cube.position.x = 5
 scene.add(cube)
 
 const sphere = new THREE.Mesh(sphereGeometry, material)
-sphere.position.x = -5
+sphere.position.x = 3
 scene.add(sphere)
 
 const icosahedron = new THREE.Mesh(icosahedronGeometry, material)
+icosahedron.position.x = 0
 scene.add(icosahedron)
+
+const plane = new THREE.Mesh(planeGeometry, material)
+plane.position.x = -2
+scene.add(plane)
+
+const torusKnot = new THREE.Mesh(torusKnotGeometry, material)
+torusKnot.position.x = -5
+scene.add(torusKnot)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -54,146 +63,34 @@ function onWindowResize() {
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
+// const options = {
+//     side: {
+//         "FrontSide": THREE.FrontSide,
+//         "BackSide": THREE.BackSide,
+//         "DoubleSide": THREE.DoubleSide,
+//     }
+// }
+
 const gui = new GUI()
-const cubeFolder = gui.addFolder('Cube')
-const cubeRotationFolder = cubeFolder.addFolder('Rotation')
-cubeRotationFolder.add(cube.rotation, 'x', 0, Math.PI * 2, 0.01)
-cubeRotationFolder.add(cube.rotation, 'y', 0, Math.PI * 2, 0.01)
-cubeRotationFolder.add(cube.rotation, 'z', 0, Math.PI * 2, 0.01)
-const cubePositionFolder = cubeFolder.addFolder('Position')
-cubePositionFolder.add(cube.position, 'x', -10, 10)
-cubePositionFolder.add(cube.position, 'y', -10, 10)
-cubePositionFolder.add(cube.position, 'z', -10, 10)
-const cubeScaleFolder = cubeFolder.addFolder('Scale')
-cubeScaleFolder
-    .add(cube.scale, 'x', -5, 5, 0.1)
-    .onFinishChange(() => console.dir(cube.geometry))
-cubeScaleFolder.add(cube.scale, 'y', -5, 5, 0.1)
-cubeScaleFolder.add(cube.scale, 'z', -5, 5, 0.1)
-cubeFolder.add(cube, 'visible', true)
-cubeFolder.open()
+const materialFolder = gui.addFolder('THREE.Material')
+// materialFolder.add(material, 'transparent').onChange(() => material.needsUpdate = true)
+// materialFolder.add(material, 'opacity', 0, 1, 0.01)
+// materialFolder.add(material, 'depthTest')
+// materialFolder.add(material, 'depthWrite')
+// materialFolder.add(material, 'alphaTest', 0, 1, 0.01).onChange(() => updateMaterial())
+// materialFolder.add(material, 'visible')
+// materialFolder.add(material, 'side', options.side).onChange(() => updateMaterial())
+materialFolder.open()
 
-const cubeData = {
-    width: 1,
-    height: 1,
-    depth: 1,
-    widthSegments: 1,
-    heightSegments: 1,
-    depthSegments: 1,
-}
-const cubePropertiesFolder = cubeFolder.addFolder('Properties')
-cubePropertiesFolder
-    .add(cubeData, 'width', 1, 30)
-    .onChange(regenerateBoxGeometry)
-    .onFinishChange(() => console.dir(cube.geometry))
-cubePropertiesFolder
-    .add(cubeData, 'height', 1, 30)
-    .onChange(regenerateBoxGeometry)
-cubePropertiesFolder
-    .add(cubeData, 'depth', 1, 30)
-    .onChange(regenerateBoxGeometry)
-cubePropertiesFolder
-    .add(cubeData, 'widthSegments', 1, 30)
-    .onChange(regenerateBoxGeometry)
-cubePropertiesFolder
-    .add(cubeData, 'heightSegments', 1, 30)
-    .onChange(regenerateBoxGeometry)
-cubePropertiesFolder
-    .add(cubeData, 'depthSegments', 1, 30)
-    .onChange(regenerateBoxGeometry)
-
-function regenerateBoxGeometry() {
-    const newGeometry = new THREE.BoxGeometry(
-        cubeData.width,
-        cubeData.height,
-        cubeData.depth,
-        cubeData.widthSegments,
-        cubeData.heightSegments,
-        cubeData.depthSegments
-    )
-    cube.geometry.dispose() // important !!!
-    cube.geometry = newGeometry
-}
-
-const sphereData = {
-    radius: 1,
-    widthSegments: 8,
-    heightSegments: 6,
-    phiStart: 0,
-    phiLength: Math.PI * 2,
-    thetaStart: 0,
-    thetaLength: Math.PI,
-}
-const sphereFolder = gui.addFolder('Sphere')
-const spherePropertiesFolder = sphereFolder.addFolder('Properties')
-spherePropertiesFolder
-    .add(sphereData, 'radius', 0.1, 30)
-    .onChange(regenerateSphereGeometry)
-spherePropertiesFolder
-    .add(sphereData, 'widthSegments', 1, 32)
-    .onChange(regenerateSphereGeometry)
-spherePropertiesFolder
-    .add(sphereData, 'heightSegments', 1, 16)
-    .onChange(regenerateSphereGeometry)
-spherePropertiesFolder
-    .add(sphereData, 'phiStart', 0, Math.PI * 2)
-    .onChange(regenerateSphereGeometry)
-spherePropertiesFolder
-    .add(sphereData, 'phiLength', 0, Math.PI * 2)
-    .onChange(regenerateSphereGeometry)
-spherePropertiesFolder
-    .add(sphereData, 'thetaStart', 0, Math.PI)
-    .onChange(regenerateSphereGeometry)
-spherePropertiesFolder
-    .add(sphereData, 'thetaLength', 0, Math.PI)
-    .onChange(regenerateSphereGeometry)
-
-function regenerateSphereGeometry() {
-    const newGeometry = new THREE.SphereGeometry(
-        sphereData.radius,
-        sphereData.widthSegments,
-        sphereData.heightSegments,
-        sphereData.phiStart,
-        sphereData.phiLength,
-        sphereData.thetaStart,
-        sphereData.thetaLength
-    )
-    sphere.geometry.dispose()
-    sphere.geometry = newGeometry
-}
-
-const icosahedronData = {
-    radius: 1,
-    detail: 0,
-}
-const icosahedronFolder = gui.addFolder('Icosahedron')
-const icosahedronPropertiesFolder = icosahedronFolder.addFolder('Properties')
-icosahedronPropertiesFolder
-    .add(icosahedronData, 'radius', 0.1, 10)
-    .onChange(regenerateIcosahedronGeometry)
-icosahedronPropertiesFolder
-    .add(icosahedronData, 'detail', 0, 5)
-    .step(1)
-    .onChange(regenerateIcosahedronGeometry)
-
-function regenerateIcosahedronGeometry() {
-    const newGeometry = new THREE.IcosahedronGeometry(
-        icosahedronData.radius,
-        icosahedronData.detail
-    )
-    icosahedron.geometry.dispose()
-    icosahedron.geometry = newGeometry
-}
-
-const debug = document.getElementById('debug1') as HTMLDivElement
+// function updateMaterial() {
+//     material.side = Number(material.side) as THREE.Side
+//     material.needsUpdate = true
+// }
 
 function animate() {
     requestAnimationFrame(animate)
 
     render()
-
-    debug.innerText =
-        'Matrix\n' + cube.matrix.elements.toString().replace(/,/g, '\n')
 
     stats.update()
 }
