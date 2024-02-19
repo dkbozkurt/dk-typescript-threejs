913204bebb72fc9ae7e9f15275b3b03bfbe86fdc
Dogukan Bozkurt <dkaanbozkurt@gmail.com>
2024-02-19 17:47:29 +0100

    Mesh Standart Material - start script

--- Three.js-TypeScript-Tutorial/src/client/client.ts
+++ Three.js-TypeScript-Tutorial/src/client/client.ts
@@ -10,10 +10,6 @@ const light = new THREE.PointLight(0xffffff, 1000)
 light.position.set(10, 10, 10)
 scene.add(light)
 
-const light2 = new THREE.PointLight(0xffffff, 1000)
-light2.position.set(-10, -10, -10)
-scene.add(light2)
-
 const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
@@ -34,14 +30,17 @@ const icosahedronGeometry = new THREE.IcosahedronGeometry(1, 0)
 const planeGeometry = new THREE.PlaneGeometry()
 const torusKnotGeometry = new THREE.TorusKnotGeometry()
 
-const material = new THREE.MeshPhongMaterial()
+const material = new THREE.MeshStandardMaterial()
 
-// const texture = new THREE.TextureLoader().load("img/grid.png")
+// const texture = new THREE.TextureLoader().load('img/grid.png')
 // material.map = texture
-// const envTexture = new THREE.CubeTextureLoader().load(["img/px_50.png", "img/nx_50.png", "img/py_50.png", "img/ny_50.png", "img/pz_50.png", "img/nz_50.png"])
-// //envTexture.mapping = THREE.CubeReflectionMapping
-// envTexture.mapping = THREE.CubeRefractionMapping
-// material.envMap = envTexture
+// const pmremGenerator = new THREE.PMREMGenerator(renderer)
+// const envTexture = new THREE.CubeTextureLoader().load(['img/px_50.png','img/nx_50.png','img/py_50.png','img/ny_50.png','img/pz_50.png','img/nz_50.png'],
+//     () => {
+//         material.envMap = pmremGenerator.fromCubemap(envTexture).texture
+//         pmremGenerator.dispose()
+//     }
+// )
 
 const cube = new THREE.Mesh(boxGeometry, material)
 cube.position.x = 5
@@ -80,11 +79,6 @@ const options = {
         BackSide: THREE.BackSide,
         DoubleSide: THREE.DoubleSide,
     },
-    combine: {
-        MultiplyOperation: THREE.MultiplyOperation,
-        MixOperation: THREE.MixOperation,
-        AddOperation: THREE.AddOperation,
-    },
 }
 
 const gui = new GUI()
@@ -105,35 +99,28 @@ materialFolder.open()
 const data = {
     color: material.color.getHex(),
     emissive: material.emissive.getHex(),
-    specular: material.specular.getHex()
 }
 
-const meshPhongMaterialFolder = gui.addFolder('THREE.MeshPhongMaterial')
-meshPhongMaterialFolder.addColor(data, 'color').onChange(() => {
+const meshStandardMaterialFolder = gui.addFolder('THREE.MeshStandardMaterial')
+
+meshStandardMaterialFolder.addColor(data, 'color').onChange(() => {
     material.color.setHex(Number(data.color.toString().replace('#', '0x')))
 })
-meshPhongMaterialFolder.addColor(data, 'emissive').onChange(() => {
+meshStandardMaterialFolder.addColor(data, 'emissive').onChange(() => {
     material.emissive.setHex(
         Number(data.emissive.toString().replace('#', '0x'))
     )
 })
-meshPhongMaterialFolder.addColor(data, 'specular').onChange(() => { material.specular.setHex(Number(data.specular.toString().replace('#', '0x'))) });
-meshPhongMaterialFolder.add(material, 'shininess', 0, 1024);
-meshPhongMaterialFolder.add(material, 'wireframe')
-meshPhongMaterialFolder.add(material, 'wireframeLinewidth', 0, 10)
-meshPhongMaterialFolder
+meshStandardMaterialFolder.add(material, 'wireframe')
+meshStandardMaterialFolder
     .add(material, 'flatShading')
     .onChange(() => updateMaterial())
-meshPhongMaterialFolder
-    .add(material, 'combine', options.combine)
-    .onChange(() => updateMaterial())
-meshPhongMaterialFolder.add(material, 'reflectivity', 0, 1)
-meshPhongMaterialFolder.add(material, 'refractionRatio', 0, 1)
-meshPhongMaterialFolder.open()
+//meshStandardMaterialFolder.add(material, 'roughness', 0, 1)
+//meshStandardMaterialFolder.add(material, 'metalness', 0, 1)
+meshStandardMaterialFolder.open()
 
 function updateMaterial() {
     material.side = Number(material.side) as THREE.Side
-    material.combine = Number(material.combine) as THREE.Combine
     material.needsUpdate = true
 }
 
