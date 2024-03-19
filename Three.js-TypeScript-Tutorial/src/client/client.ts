5dd6fe78b36adc7ce103ffec2eea2f82d19ac020
Dogukan Bozkurt <dkaanbozkurt@gmail.com>
2024-03-19 12:55:57 +0100

    Specular Map - shining texture

--- Three.js-TypeScript-Tutorial/src/client/client.ts
+++ Three.js-TypeScript-Tutorial/src/client/client.ts
@@ -6,8 +6,8 @@ import { GUI } from 'dat.gui'
 const scene = new THREE.Scene()
 scene.add(new THREE.AxesHelper(5))
 
-const light = new THREE.PointLight(0xffffff, 500)
-light.position.set(10, 10, 10)
+const light = new THREE.PointLight(0xffffff, 1000)
+light.position.set(0, 5, 10)
 scene.add(light)
 
 const camera = new THREE.PerspectiveCamera(
@@ -22,50 +22,28 @@ const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement)
+//controls.screenSpacePanning = true // default is now true since three r118. Used so that panning up and down doesn't zoom in/out
+//controls.addEventListener('change', render)
 
-const boxGeometry = new THREE.BoxGeometry()
-const sphereGeometry = new THREE.SphereGeometry()
-const icosahedronGeometry = new THREE.IcosahedronGeometry(1, 0)
-const planeGeometry = new THREE.PlaneGeometry()
-const torusKnotGeometry = new THREE.TorusKnotGeometry()
+const planeGeometry = new THREE.PlaneGeometry() //3.6, 1.8)
 
-const threeTone = new THREE.TextureLoader().load('img/threeTone.jpg')
-threeTone.minFilter = THREE.NearestFilter
-threeTone.magFilter = THREE.NearestFilter
+const material = new THREE.MeshPhongMaterial()
 
-const fourTone = new THREE.TextureLoader().load('img/fourTone.jpg')
-fourTone.minFilter = THREE.NearestFilter
-fourTone.magFilter = THREE.NearestFilter
+const texture = new THREE.TextureLoader().load('img/grid.png')
+//const texture = new THREE.TextureLoader().load("img/worldColour.5400x2700.jpg")
+material.map = texture
+// const envTexture = new THREE.CubeTextureLoader().load(["img/px_50.png", "img/nx_50.png", "img/py_50.png", "img/ny_50.png", "img/pz_50.png", "img/nz_50.png"])
+//const envTexture = new THREE.CubeTextureLoader().load(["img/px_eso0932a.jpg", "img/nx_eso0932a.jpg", "img/py_eso0932a.jpg", "img/ny_eso0932a.jpg", "img/pz_eso0932a.jpg", "img/nz_eso0932a.jpg"])
+// envTexture.mapping = THREE.CubeReflectionMapping
+// material.envMap = envTexture
 
-const fiveTone = new THREE.TextureLoader().load('img/fiveTone.jpg')
-fiveTone.minFilter = THREE.NearestFilter
-fiveTone.magFilter = THREE.NearestFilter
-
-// For custom textures if we want to make it smooth, we basically need to remove min and max filers.
-const smoothThreeTone = new THREE.TextureLoader().load('img/ThreeTone.jpg')
-
-const material: THREE.MeshToonMaterial = new THREE.MeshToonMaterial()
-
-const cube = new THREE.Mesh(boxGeometry, material)
-cube.position.x = 5
-scene.add(cube)
-
-const sphere = new THREE.Mesh(sphereGeometry, material)
-sphere.position.x = 3
-scene.add(sphere)
-
-const icosahedron = new THREE.Mesh(icosahedronGeometry, material)
-icosahedron.position.x = 0
-scene.add(icosahedron)
+const specularTexture = new THREE.TextureLoader().load("img/grayscale-test.png")
+//const specularTexture = new THREE.TextureLoader().load("img/earthSpecular.jpg")
+material.specularMap = specularTexture
 
 const plane = new THREE.Mesh(planeGeometry, material)
-plane.position.x = -2
 scene.add(plane)
-
-const torusKnot = new THREE.Mesh(torusKnotGeometry, material)
-torusKnot.position.x = -5
-scene.add(torusKnot)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -84,35 +62,16 @@ const options = {
         BackSide: THREE.BackSide,
         DoubleSide: THREE.DoubleSide,
     },
-    gradientMap: {
-        Default: null,
-        threeTone: 'threeTone',
-        fourTone: 'fourTone',
-        fiveTone: 'fiveTone',
-        smoothThreeTone:'smoothThreeTone',
+    combine: {
+        MultiplyOperation: THREE.MultiplyOperation,
+        MixOperation: THREE.MixOperation,
+        AddOperation: THREE.AddOperation,
     },
 }
-
 const gui = new GUI()
 
-const data = {
-    lightColor: light.color.getHex(),
-    color: material.color.getHex(),
-    gradientMap: 'threeTone',
-}
-
-material.gradientMap = threeTone
-
-const lightFolder = gui.addFolder('THREE.Light')
-lightFolder.addColor(data, 'lightColor').onChange(() => {
-    light.color.setHex(Number(data.lightColor.toString().replace('#', '0x')))
-})
-lightFolder.add(light, 'intensity', 0, 2000)
-
 const materialFolder = gui.addFolder('THREE.Material')
-materialFolder
-    .add(material, 'transparent')
-    .onChange(() => (material.needsUpdate = true))
+materialFolder.add(material, 'transparent')
 materialFolder.add(material, 'opacity', 0, 1, 0.01)
 materialFolder.add(material, 'depthTest')
 materialFolder.add(material, 'depthWrite')
@@ -125,21 +84,41 @@ materialFolder
     .onChange(() => updateMaterial())
 //materialFolder.open()
 
-const meshToonMaterialFolder = gui.addFolder('THREE.MeshToonMaterial')
-meshToonMaterialFolder.addColor(data, 'color').onChange(() => {
+const data = {
+    color: material.color.getHex(),
+    emissive: material.emissive.getHex(),
+    specular: material.specular.getHex(),
+}
+
+const meshPhongMaterialFolder = gui.addFolder('THREE.MeshPhongMaterial')
+
+meshPhongMaterialFolder.addColor(data, 'color').onChange(() => {
     material.color.setHex(Number(data.color.toString().replace('#', '0x')))
 })
-
-//shininess, specular and flatShading no longer supported in MeshToonMaterial
-
-meshToonMaterialFolder
-    .add(data, 'gradientMap', options.gradientMap)
+meshPhongMaterialFolder.addColor(data, 'emissive').onChange(() => {
+    material.emissive.setHex(
+        Number(data.emissive.toString().replace('#', '0x'))
+    )
+})
+meshPhongMaterialFolder.addColor(data, 'specular').onChange(() => {
+    material.specular.setHex(
+        Number(data.specular.toString().replace('#', '0x'))
+    )
+})
+meshPhongMaterialFolder.add(material, 'shininess', 0, 1024)
+meshPhongMaterialFolder.add(material, 'wireframe')
+meshPhongMaterialFolder
+    .add(material, 'flatShading')
     .onChange(() => updateMaterial())
-meshToonMaterialFolder.open()
+meshPhongMaterialFolder
+    .add(material, 'combine', options.combine)
+    .onChange(() => updateMaterial())
+meshPhongMaterialFolder.add(material, 'reflectivity', 0, 1)
+meshPhongMaterialFolder.open()
 
 function updateMaterial() {
     material.side = Number(material.side) as THREE.Side
-    material.gradientMap = eval(data.gradientMap as string)
+    material.combine = Number(material.combine) as THREE.Combine
     material.needsUpdate = true
 }
 
