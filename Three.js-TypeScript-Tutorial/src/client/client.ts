801109ef925166e2b415b8ff9118c08cc7da376c
Dogukan Bozkurt <dkaanbozkurt@gmail.com>
2024-03-21 17:32:42 +0100

    Bump Map

--- Three.js-TypeScript-Tutorial/src/client/client.ts
+++ Three.js-TypeScript-Tutorial/src/client/client.ts
@@ -16,41 +16,27 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
-camera.position.z = 3
+camera.position.z = 1
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
-controls.screenSpacePanning = true //so that panning up and down doesn't zoom in/out
-//controls.addEventListener('change', render)
+controls.enableDamping = true
 
 const planeGeometry = new THREE.PlaneGeometry(3.6, 1.8)
 
-const material = new THREE.MeshPhysicalMaterial({})
+const material = new THREE.MeshPhongMaterial()
 
-//const texture = new THREE.TextureLoader().load("img/grid.png")
 const texture = new THREE.TextureLoader().load('img/worldColour.5400x2700.jpg')
 material.map = texture
-// const envTexture = new THREE.CubeTextureLoader().load(["img/px_50.png", "img/nx_50.png", "img/py_50.png", "img/ny_50.png", "img/pz_50.png", "img/nz_50.png"])
-const envTexture = new THREE.CubeTextureLoader().load([
-    'img/px_eso0932a.jpg',
-    'img/nx_eso0932a.jpg',
-    'img/py_eso0932a.jpg',
-    'img/ny_eso0932a.jpg',
-    'img/pz_eso0932a.jpg',
-    'img/nz_eso0932a.jpg',
-])
-envTexture.mapping = THREE.CubeReflectionMapping
-material.envMap = envTexture
 
-//const specularTexture = new THREE.TextureLoader().load("img/grayscale-test.png")
-const specularTexture = new THREE.TextureLoader().load('img/earthSpecular.jpg')
-material.roughnessMap = specularTexture
-material.metalnessMap = specularTexture
+const bumpTexture = new THREE.TextureLoader().load('img/earth_bumpmap.jpg')
+material.bumpMap = bumpTexture
+material.bumpScale = 0.015
 
-const plane: THREE.Mesh = new THREE.Mesh(planeGeometry, material)
+const plane = new THREE.Mesh(planeGeometry, material)
 scene.add(plane)
 
 window.addEventListener('resize', onWindowResize, false)
@@ -64,65 +50,13 @@ function onWindowResize() {
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
-const options = {
-    side: {
-        FrontSide: THREE.FrontSide,
-        BackSide: THREE.BackSide,
-        DoubleSide: THREE.DoubleSide,
-    },
-}
 const gui = new GUI()
-
-const materialFolder = gui.addFolder('THREE.Material')
-materialFolder.add(material, 'transparent')
-materialFolder.add(material, 'opacity', 0, 1, 0.01)
-materialFolder.add(material, 'depthTest')
-materialFolder.add(material, 'depthWrite')
-materialFolder
-    .add(material, 'alphaTest', 0, 1, 0.01)
-    .onChange(() => updateMaterial())
-materialFolder.add(material, 'visible')
-materialFolder
-    .add(material, 'side', options.side)
-    .onChange(() => updateMaterial())
-//materialFolder.open()
-
-const data = {
-    color: material.color.getHex(),
-    emissive: material.emissive.getHex(),
-}
-
-const meshPhysicalMaterialFolder = gui.addFolder(
-    'THREE.meshPhysicalMaterialFolder'
-)
-
-meshPhysicalMaterialFolder.addColor(data, 'color').onChange(() => {
-    material.color.setHex(Number(data.color.toString().replace('#', '0x')))
-})
-meshPhysicalMaterialFolder.addColor(data, 'emissive').onChange(() => {
-    material.emissive.setHex(
-        Number(data.emissive.toString().replace('#', '0x'))
-    )
-})
-meshPhysicalMaterialFolder.add(material, 'wireframe')
-meshPhysicalMaterialFolder
-    .add(material, 'flatShading')
-    .onChange(() => updateMaterial())
-meshPhysicalMaterialFolder.add(material, 'reflectivity', 0, 1)
-meshPhysicalMaterialFolder.add(material, 'envMapIntensity', 0, 1)
-meshPhysicalMaterialFolder.add(material, 'roughness', 0, 1)
-meshPhysicalMaterialFolder.add(material, 'metalness', 0, 1)
-meshPhysicalMaterialFolder.add(material, 'clearcoat', 0, 1, 0.01)
-meshPhysicalMaterialFolder.add(material, 'clearcoatRoughness', 0, 1, 0.01)
-meshPhysicalMaterialFolder.open()
-
-function updateMaterial() {
-    material.side = Number(material.side) as THREE.Side
-    material.needsUpdate = true
-}
+gui.add(material, 'bumpScale', 0, 100, 0.01)
 
 function animate() {
     requestAnimationFrame(animate)
+
+    controls.update()
 
     render()
 
