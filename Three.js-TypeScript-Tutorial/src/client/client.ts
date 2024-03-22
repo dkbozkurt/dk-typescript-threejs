4d6de1b43ebf5f8b5b31465f7fac27afe37d7b3e
Dogukan Bozkurt <dkaanbozkurt@gmail.com>
2024-03-22 17:47:46 +0100

    DisplacementMap with Shadow

--- Three.js-TypeScript-Tutorial/src/client/client.ts
+++ Three.js-TypeScript-Tutorial/src/client/client.ts
@@ -1,17 +1,25 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import Stats from 'three/examples/jsm/libs/stats.module'
 import { GUI } from 'dat.gui'
+import Stats from 'three/examples/jsm/libs/stats.module'
 
 const scene = new THREE.Scene()
-scene.add(new THREE.AxesHelper(5))
+const axesHelper = new THREE.AxesHelper(5)
+scene.add(axesHelper)
 
-const light = new THREE.DirectionalLight(0xffffff, Math.PI)
+const light = new THREE.DirectionalLight(0xffffff, 10)
+light.position.set(-4.4, 3.3, 2.2)
 light.castShadow = true
-light.shadow.mapSize.width = 512
-light.shadow.mapSize.height = 512
-light.shadow.camera.near = 0.5
-light.shadow.camera.far = 100
+light.shadow.bias = -0.003
+light.shadow.mapSize.width = 2048
+light.shadow.mapSize.height = 2048
+light.shadow.camera.left = -5
+light.shadow.camera.right = 5
+light.shadow.camera.top = 2
+light.shadow.camera.bottom = -2
+light.shadow.camera.near = 1
+light.shadow.camera.far = 10
+
 scene.add(light)
 
 //const helper = new THREE.DirectionalLightHelper(light);
@@ -24,80 +32,40 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
-camera.position.z = 7
+camera.position.set(0, 1, 1)
 
 const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFSoftShadowMap
-//renderer.shadowMap.type = THREE.BasicShadowMap
-//renderer.shadowMap.type = THREE.PCFShadowMap
-//renderer.shadowMap.type = THREE.VSMShadowMap
+renderer.shadowMap.type = THREE.PCFShadowMap
+//renderer.physicallyCorrectLights = true //deprecated
+renderer.useLegacyLights = false //use this instead of setting physicallyCorrectLights=true property
+renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
 
-const planeGeometry = new THREE.PlaneGeometry(100, 20)
-const plane = new THREE.Mesh(planeGeometry, new THREE.MeshPhongMaterial())
-plane.rotateX(-Math.PI / 2)
-plane.position.y = -1.75
+const planeGeometry = new THREE.PlaneGeometry(3.6, 1.8, 360, 180)
+
+const material = new THREE.MeshStandardMaterial()
+material.metalness = 0
+material.roughness = 0
+
+const texture = new THREE.TextureLoader().load('img/worldColour.5400x2700.jpg')
+material.map = texture
+
+const displacementMap = new THREE.TextureLoader().load(
+    'img/gebco_bathy.5400x2700_8bit.jpg'
+)
+material.displacementMap = displacementMap
+material.displacementScale = 0.3
+
+const plane = new THREE.Mesh(planeGeometry, material)
+plane.rotation.x = -Math.PI / 2
+plane.castShadow = true
 plane.receiveShadow = true
+
 scene.add(plane)
-
-const torusGeometry = [
-    new THREE.TorusGeometry(),
-    new THREE.TorusGeometry(),
-    new THREE.TorusGeometry(),
-    new THREE.TorusGeometry(),
-    new THREE.TorusGeometry(),
-]
-
-const material = [
-    new THREE.MeshBasicMaterial(),
-    new THREE.MeshLambertMaterial(),
-    new THREE.MeshPhongMaterial(),
-    new THREE.MeshPhysicalMaterial({}),
-    new THREE.MeshToonMaterial(),
-]
-
-const torus = [
-    new THREE.Mesh(torusGeometry[0], material[0]),
-    new THREE.Mesh(torusGeometry[1], material[1]),
-    new THREE.Mesh(torusGeometry[2], material[2]),
-    new THREE.Mesh(torusGeometry[3], material[3]),
-    new THREE.Mesh(torusGeometry[4], material[4]),
-]
-
-const texture = new THREE.TextureLoader().load('img/grid.png')
-material[0].map = texture
-material[1].map = texture
-material[2].map = texture
-material[3].map = texture
-material[4].map = texture
-
-torus[0].position.x = -8
-torus[1].position.x = -4
-torus[2].position.x = 0
-torus[3].position.x = 4
-torus[4].position.x = 8
-
-torus[0].castShadow = true
-torus[1].castShadow = true
-torus[2].castShadow = true
-torus[3].castShadow = true
-torus[4].castShadow = true
-
-torus[0].receiveShadow = true
-torus[1].receiveShadow = true
-torus[2].receiveShadow = true
-torus[3].receiveShadow = true
-torus[4].receiveShadow = true
-
-scene.add(torus[0])
-scene.add(torus[1])
-scene.add(torus[2])
-scene.add(torus[3])
-scene.add(torus[4])
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -106,36 +74,82 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight)
     render()
 }
-
 const stats = new Stats()
 document.body.appendChild(stats.dom)
-
+var options = {
+    side: {
+        FrontSide: THREE.FrontSide,
+        BackSide: THREE.BackSide,
+        DoubleSide: THREE.DoubleSide,
+    },
+}
+const gui = new GUI()
+const materialFolder = gui.addFolder('THREE.Material')
+materialFolder
+    .add(material, 'transparent')
+    .onChange(() => (material.needsUpdate = true))
+materialFolder.add(material, 'opacity', 0, 1, 0.01)
+materialFolder.add(material, 'depthTest')
+materialFolder.add(material, 'depthWrite')
+materialFolder
+    .add(material, 'alphaTest', 0, 1, 0.01)
+    .onChange(() => updateMaterial())
+materialFolder.add(material, 'visible')
+materialFolder
+    .add(material, 'side', options.side)
+    .onChange(() => updateMaterial())
+//materialFolder.open()
 const data = {
-    color: light.color.getHex(),
-    mapsEnabled: true,
-    shadowMapSizeWidth: 512,
-    shadowMapSizeHeight: 512,
+    color: material.color.getHex(),
+    shadowMapSizeWidth: 2048,
+    shadowMapSizeHeight: 2048,
 }
 
-const gui = new GUI()
-const lightFolder = gui.addFolder('THREE.Light')
-lightFolder.addColor(data, 'color').onChange(() => {
-    light.color.setHex(Number(data.color.toString().replace('#', '0x')))
+const meshStandardMaterialFolder = gui.addFolder('THREE.MeshStandardMaterial')
+
+meshStandardMaterialFolder.addColor(data, 'color').onChange(() => {
+    material.color.setHex(Number(data.color.toString().replace('#', '0x')))
 })
-lightFolder.add(light, 'intensity', 0, Math.PI * 2, 0.01)
+meshStandardMaterialFolder.add(material, 'wireframe')
+meshStandardMaterialFolder
+    .add(material, 'flatShading')
+    .onChange(() => updateMaterial())
+meshStandardMaterialFolder.add(material, 'displacementScale', -1, 1, 0.01)
+meshStandardMaterialFolder.add(material, 'displacementBias', -1, 1, 0.01)
+
+meshStandardMaterialFolder.add(material, 'roughness', 0, 1)
+meshStandardMaterialFolder.add(material, 'metalness', 0, 1)
+//meshStandardMaterialFolder.open()
+
+var planeData = {
+    width: 3.6,
+    height: 1.8,
+    widthSegments: 180,
+    heightSegments: 90,
+}
+const planePropertiesFolder = gui.addFolder('PlaneGeometry')
+//planePropertiesFolder.add(planeData, 'width', 1, 30).onChange(regeneratePlaneGeometry)
+//planePropertiesFolder.add(planeData, 'height', 1, 30).onChange(regeneratePlaneGeometry)
+planePropertiesFolder
+    .add(planeData, 'widthSegments', 1, 360)
+    .onChange(regeneratePlaneGeometry)
+planePropertiesFolder
+    .add(planeData, 'heightSegments', 1, 180)
+    .onChange(regeneratePlaneGeometry)
+//planePropertiesFolder.open()
 
 const directionalLightFolder = gui.addFolder('THREE.DirectionalLight')
 directionalLightFolder
-    .add(light.shadow.camera, 'left', -10, -1, 0.1)
+    .add(light.shadow.camera, 'left', -5, -1, 0.1)
     .onChange(() => light.shadow.camera.updateProjectionMatrix())
 directionalLightFolder
-    .add(light.shadow.camera, 'right', 1, 10, 0.1)
+    .add(light.shadow.camera, 'right', 1, 5, 0.1)
     .onChange(() => light.shadow.camera.updateProjectionMatrix())
 directionalLightFolder
-    .add(light.shadow.camera, 'top', 1, 10, 0.1)
+    .add(light.shadow.camera, 'top', 1, 5, 0.1)
     .onChange(() => light.shadow.camera.updateProjectionMatrix())
 directionalLightFolder
-    .add(light.shadow.camera, 'bottom', -10, -1, 0.1)
+    .add(light.shadow.camera, 'bottom', -5, -1, 0.1)
     .onChange(() => light.shadow.camera.updateProjectionMatrix())
 directionalLightFolder
     .add(light.shadow.camera, 'near', 0.1, 100)
@@ -149,9 +163,9 @@ directionalLightFolder
 directionalLightFolder
     .add(data, 'shadowMapSizeHeight', [256, 512, 1024, 2048, 4096])
     .onChange(() => updateShadowMapSize())
-directionalLightFolder.add(light.position, 'x', -50, 50, 0.01)
-directionalLightFolder.add(light.position, 'y', -50, 50, 0.01)
-directionalLightFolder.add(light.position, 'z', -50, 50, 0.01)
+directionalLightFolder.add(light.position, 'x', -5, 5, 0.01)
+directionalLightFolder.add(light.position, 'y', -5, 5, 0.01)
+directionalLightFolder.add(light.position, 'z', -5, 5, 0.01)
 directionalLightFolder.open()
 
 function updateShadowMapSize() {
@@ -160,34 +174,29 @@ function updateShadowMapSize() {
     ;(light.shadow.map as any) = null
 }
 
-const meshesFolder = gui.addFolder('Meshes')
-meshesFolder.add(data, 'mapsEnabled').onChange(() => {
-    material.forEach((m) => {
-        if (data.mapsEnabled) {
-            m.map = texture
-        } else {
-            m.map = null
-        }
-        m.needsUpdate = true
-    })
-})
+function regeneratePlaneGeometry() {
+    let newGeometry = new THREE.PlaneGeometry(
+        planeData.width,
+        planeData.height,
+        planeData.widthSegments,
+        planeData.heightSegments
+    )
+    plane.geometry.dispose()
+    plane.geometry = newGeometry
+}
+function updateMaterial() {
+    material.side = Number(material.side) as THREE.Side
+    material.needsUpdate = true
+}
 
 function animate() {
     requestAnimationFrame(animate)
-
     helper.update()
-
-    torus.forEach((t) => {
-        t.rotation.y += 0.01
-    })
-
+    controls.update()
     render()
-
     stats.update()
 }
-
 function render() {
     renderer.render(scene, camera)
 }
-
 animate()
