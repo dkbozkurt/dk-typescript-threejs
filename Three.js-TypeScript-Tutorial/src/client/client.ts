bd520bf5fd3367ad6bd9347b62a79ad474d87ac6
Dogukan Bozkurt <dkaanbozkurt@gmail.com>
2024-03-22 17:37:10 +0100

    Spot Light

--- Three.js-TypeScript-Tutorial/src/client/client.ts
+++ Three.js-TypeScript-Tutorial/src/client/client.ts
@@ -6,10 +6,10 @@ import { GUI } from 'dat.gui'
 const scene = new THREE.Scene()
 scene.add(new THREE.AxesHelper(5))
 
-const light = new THREE.PointLight(0xffffff, 2)
+const light = new THREE.SpotLight(0xffffff, 2)
 scene.add(light)
 
-const helper = new THREE.PointLightHelper(light)
+const helper = new THREE.SpotLightHelper(light)
 scene.add(helper)
 
 const camera = new THREE.PerspectiveCamera(
@@ -26,11 +26,11 @@ document.body.appendChild(renderer.domElement)
 
 new OrbitControls(camera, renderer.domElement)
 
-// const planeGeometry = new THREE.PlaneGeometry(20, 10)//, 360, 180)
-// const plane = new THREE.Mesh(planeGeometry, new THREE.MeshPhongMaterial())
-// plane.rotateX(-Math.PI / 2)
-// //plane.position.y = -1.75
-// scene.add(plane)
+const planeGeometry = new THREE.PlaneGeometry(100, 10)
+const plane = new THREE.Mesh(planeGeometry, new THREE.MeshPhongMaterial())
+plane.rotateX(-Math.PI / 2)
+plane.position.y = -1.75
+scene.add(plane)
 
 const torusGeometry = [
     new THREE.TorusGeometry(),
@@ -97,15 +97,16 @@ lightFolder.addColor(data, 'color').onChange(() => {
     light.color.setHex(Number(data.color.toString().replace('#', '0x')))
 })
 lightFolder.add(light, 'intensity', 0, 10, 0.01)
-
-const pointLightFolder = gui.addFolder('THREE.PointLight')
-pointLightFolder.add(light, 'distance', 0, 100, 0.01)
-pointLightFolder.add(light, 'decay', 0, 4, 0.1)
-pointLightFolder.add(light.position, 'x', -50, 50, 0.01)
-pointLightFolder.add(light.position, 'y', -50, 50, 0.01)
-pointLightFolder.add(light.position, 'z', -50, 50, 0.01)
-pointLightFolder.open()
-
+lightFolder.open()
+const spotLightFolder = gui.addFolder('THREE.SpotLight')
+spotLightFolder.add(light, 'distance', 0, 100, 0.01)
+spotLightFolder.add(light, 'decay', 0, 4, 0.1)
+spotLightFolder.add(light, 'angle', 0, 1, 0.1)
+spotLightFolder.add(light, 'penumbra', 0, 1, 0.1)
+spotLightFolder.add(light.position, 'x', -50, 50, 0.01)
+spotLightFolder.add(light.position, 'y', -50, 50, 0.01)
+spotLightFolder.add(light.position, 'z', -50, 50, 0.01)
+spotLightFolder.open()
 const meshesFolder = gui.addFolder('Meshes')
 meshesFolder.add(data, 'mapsEnabled').onChange(() => {
     material.forEach((m) => {
@@ -121,7 +122,7 @@ meshesFolder.add(data, 'mapsEnabled').onChange(() => {
 function animate() {
     requestAnimationFrame(animate)
 
-    //helper.update()
+    helper.update()
 
     torus.forEach((t) => {
         t.rotation.y += 0.01
