e3192937bcc2cfe9326e092d1fe51ba9039a6ad3
Dogukan Bozkurt <dkaanbozkurt@gmail.com>
2024-03-23 16:28:59 +0100

    GLTF Loader - base

--- Three.js-TypeScript-Tutorial/src/client/client.ts
+++ Three.js-TypeScript-Tutorial/src/client/client.ts
@@ -1,14 +1,13 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
-import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import Stats from 'three/examples/jsm/libs/stats.module'
 
 const scene = new THREE.Scene()
 scene.add(new THREE.AxesHelper(5))
 
-const light = new THREE.PointLight(0xffffff, 1000)
-light.position.set(2.5, 7.5, 15)
+const light = new THREE.SpotLight(0xffffff, Math.PI * 20)
+light.position.set(5, 5, 5)
 scene.add(light)
 
 const camera = new THREE.PerspectiveCamera(
@@ -17,70 +16,46 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
-camera.position.z = 3
+camera.position.z = 2
 
 const renderer = new THREE.WebGLRenderer()
+// Since Three r150, lighting has changed significantly with every version up to three r158
+// keep the threejs defaults, and reduce light watts in blender if using punctual lights
+// if using Threejs lights, then you need to experiment with light intensity.
+// renderer.physicallyCorrectLights = true //deprecated
+// renderer.useLegacyLights = false //deprecated
+renderer.shadowMap.enabled = true
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
 
-const mtlLoader = new MTLLoader()
-mtlLoader.load(
-    'models/monkey.mtl',
-    (materials) => {
-        materials.preload()
-
-        const objLoader = new OBJLoader()
-        objLoader.setMaterials(materials)
-        objLoader.load(
-            'models/monkey.obj',
-            (object) => {
-                scene.add(object)
-            },
-            (xhr) => {
-                console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-            },
-            (error) => {
-                console.log('An error happened')
-            }
-        )
+const loader = new GLTFLoader()
+loader.load(
+    'models/monkey.glb',
+    function (gltf) {
+        // gltf.scene.traverse(function (child) {
+        //     if ((child as THREE.Mesh).isMesh) {
+        //         const m = (child as THREE.Mesh)
+        //         m.receiveShadow = true
+        //         m.castShadow = true
+        //     }
+        //     if (((child as THREE.Light)).isLight) {
+        //         const l = (child as THREE.SpotLight)
+        //         l.castShadow = true
+        //         l.shadow.bias = -.003
+        //         l.shadow.mapSize.width = 2048
+        //         l.shadow.mapSize.height = 2048
+        //     }
+        // })
+        scene.add(gltf.scene)
     },
     (xhr) => {
         console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
     },
     (error) => {
-        console.log('An error happened')
-    }
-)
-
-mtlLoader.load(
-    'models/monkeyTextured.mtl',
-    (materials) => {
-        materials.preload()
-
-        const objLoader = new OBJLoader()
-        objLoader.setMaterials(materials)
-        objLoader.load(
-            'models/monkeyTextured.obj',
-            (object) => {
-                object.position.x = 2.5
-                scene.add(object)
-            },
-            (xhr) => {
-                console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-            },
-            (error) => {
-                console.log('An error happened')
-            }
-        )
-    },
-    (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-    },
-    (error) => {
-        console.log('An error happened')
+        console.log(error)
     }
 )
 
