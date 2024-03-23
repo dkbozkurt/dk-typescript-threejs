694142c55f261c3fa2f1617cb1b5e4810946fe30
Dogukan Bozkurt <dkaanbozkurt@gmail.com>
2024-03-23 15:53:08 +0100

    OBJ Modal Loader - loading multiple OBJs

--- Three.js-TypeScript-Tutorial/src/client/client.ts
+++ Three.js-TypeScript-Tutorial/src/client/client.ts
@@ -25,19 +25,34 @@ document.body.appendChild(renderer.domElement)
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
 
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
 
 const objLoader = new OBJLoader()
+objLoader.load(
+    'models/monkey.obj',
+    (object) => {
+        console.log(object);
+        // (object.children[0] as THREE.Mesh).material = material
+        // object.traverse(function (child) {
+        //     if ((child as THREE.Mesh).isMesh) {
+        //         (child as THREE.Mesh).material = material
+        //     }
+        // })
+        scene.add(object)
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+        console.log(error)
+    }
+)
+
 objLoader.load(
     'models/cube.obj',
     (object) => {
         console.log(object);
-        // (object.children[0] as THREE.Mesh).material = material
-        object.traverse(function (child) {
-            if ((child as THREE.Mesh).isMesh) {
-                (child as THREE.Mesh).material = material
-            }
-        })
+        object.position.x = -2;
         scene.add(object)
     },
     (xhr) => {
