9250ad04fac27c94197382e1bbb080667710742e
dkbozkurt <dkaanbozkurt@gmail.com>
2024-04-02 01:35:52 +0200

    Download Progress Indicator

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 <a href="https://threejs.org/" target="_blank" rel="noreferrer"> <img src="https://global.discourse-cdn.com/standard17/uploads/threejs/original/2X/e/e4f86d2200d2d35c30f7b1494e96b9595ebc2751.png" alt="threejs" width="40" height="40"/> </a> 
 <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer"> <img src="https://github.com/devicons/devicon/blob/master/icons/typescript/typescript-original.svg" alt="typescript" width="40" height="40"/> </a> 
 </p>
+
+<p>
+
+Useful Websites: https://sbcode.net/threejs/
+</p>
+
+
--- Three.js-TypeScript-Tutorial/src/client/client.ts
+++ Three.js-TypeScript-Tutorial/src/client/client.ts
@@ -1,13 +1,23 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader'
 import Stats from 'three/examples/jsm/libs/stats.module'
-import { OBB } from 'three/examples/jsm/math/OBB'
 
 const scene = new THREE.Scene()
-scene.add(new THREE.AxesHelper(5))
 
-const light = new THREE.AmbientLight()
-scene.add(light)
+new RGBELoader().load(
+    './img/kloppenheim_06_puresky_1k.hdr',
+    function (texture) {
+        texture.mapping = THREE.EquirectangularReflectionMapping
+        scene.background = texture
+        scene.environment = texture
+    },
+    (xhr) => {
+        const percentComplete = (xhr.loaded / xhr.total) * 100
+        progressBar.value = percentComplete === Infinity ? 100 : percentComplete
+    }
+)
 
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -15,7 +25,7 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
-camera.position.set(0.8, 1.4, 3.0)
+camera.position.set(1.15, 1.15, 1.15)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -23,69 +33,40 @@ document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
+controls.target.y = 1
 
-const geometry = new THREE.BoxGeometry(1, 2, 3)
-geometry.computeBoundingBox()
-const material = new THREE.MeshPhongMaterial()
-const mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(4, 1, 0)
-mesh.geometry.userData.obb = new OBB().fromBox3(
-    mesh.geometry.boundingBox as THREE.Box3
+const progressBar = document.getElementById(
+    'progressBar'
+) as HTMLProgressElement
+
+const loader = new GLTFLoader()
+loader.load(
+    'models/xbot.glb',
+    function (gltf) {
+        progressBar.style.display = 'none'
+        scene.add(gltf.scene)
+    },
+    (xhr) => {
+        const percentComplete = (xhr.loaded / xhr.total) * 100
+        progressBar.value = percentComplete === Infinity ? 100 : percentComplete
+    }
 )
-mesh.userData.obb = new OBB()
-scene.add(mesh)
 
-const mesh2 = new THREE.Mesh(
-    geometry,
-    new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
-)
-mesh2.position.set(-3, 1, 0)
-mesh2.geometry.userData.obb = new OBB().fromBox3(
-    mesh2.geometry.boundingBox as THREE.Box3
-)
-mesh2.userData.obb = new OBB()
-
-scene.add(mesh2)
-
-const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(20, 20, 10, 10),
-    new THREE.MeshBasicMaterial({ color: 0xaec6cf, wireframe: true })
-)
-floor.rotateX(-Math.PI / 2)
-scene.add(floor)
-
-window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
     render()
 }
+window.addEventListener('resize', onWindowResize, false)
 
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
-const clock = new THREE.Clock()
-
 function animate() {
     requestAnimationFrame(animate)
 
-    mesh.position.x = Math.sin(clock.getElapsedTime() * 0.5) * 4
-
     controls.update()
-
-    mesh.userData.obb.copy(mesh.geometry.userData.obb)
-    mesh2.userData.obb.copy(mesh2.geometry.userData.obb)
-    mesh.userData.obb.applyMatrix4(mesh.matrixWorld)
-    mesh2.userData.obb.applyMatrix4(mesh2.matrixWorld)
-    if (mesh.userData.obb.intersectsOBB(mesh2.userData.obb)) {
-        mesh.material.color.set(0xff0000)
-    } else {
-        mesh.material.color.set(0x00ff00)
-    }
-
-    mesh.rotateY(0.01)
-    mesh2.rotateY(-0.005)
 
     render()
 
